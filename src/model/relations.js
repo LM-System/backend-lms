10082a35eecd2430bf9796df8edddafe6c915bde
Helmi-Qatqat <helmiqatqat@gmail.com>
2023-08-01 08:29:13 +0000

    add tips

--- src/model/relations.js
+++ src/model/relations.js
@@ -15,7 +15,27 @@ const {
   announcementModel
   } = require('./index')
 
+// Users Relations
 
+// Courses Relations
+
+// Department Relations
+
+// Institution Relations
+
+// Sections Relations
+
+// Content Relations
+
+// ContentFile Relations
+
+// Prerequest Relations
+
+// Attendance Relations
+
+// Assignment Relations
+
+// Announcement Relations
 
 
 module.exports = {
