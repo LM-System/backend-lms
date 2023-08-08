31b5eab3974630b211d56280746130fdc52e0e41
Helmi-Qatqat <helmiqatqat@gmail.com>
2023-08-08 19:29:37 +0000

    profile-image-router added

---
+++ src/routes/profile-image/profile-image-router.js
@@ -0,0 +1,25 @@
+'use strict'
+const fs = require('fs');
+const {usersModel} = require('../../model/relations')
+const profileImageRouter = require('express').Router()
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' });
+
+
+profileImageRouter.post('/profile/:user_id/image', upload.single('file'), async (req, res) => {
+  const fileBuffer = fs.readFileSync(req.file.path);
+  const fileBase64String = fileBuffer.toString('base64');
+  const userId = req.params.user_id
+  const user = await usersModel.update(
+  {
+    image: fileBase64String
+  },
+  { where: {
+    id: userId
+  }})
+  if(user) {
+    res.sendStatus(200)
+  }
+})
+
+module.exports = profileImageRouter
