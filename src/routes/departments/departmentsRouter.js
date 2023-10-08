b8dde5f124692d96e92e4b05e79c38b72cae8ad2
MalekHasan <malek.alsheekh01@gmail.com>
2023-10-08 07:47:56 +0000

    done

--- src/routes/departments/departmentsRouter.js
+++ src/routes/departments/departmentsRouter.js
@@ -1,19 +1,57 @@
-const express = require('express');
-const{Sequelize}= require('sequelize')
+const express = require("express");
+const { Sequelize } = require("sequelize");
 const departmentsRouter = express.Router();
-const acl = require('../../auth/middleware/acl.auth')
-const bearer = require('../../auth/middleware/bearer.auth')
-const specificity = require('../../auth/middleware/specificity.auth')
-const head = require('../../auth/middleware/head')
-const {coursesModel,departmentsModel,usersModel, studentsModel, institutionModel, instructorsModel}= require('../../model/relations')
+const acl = require("../../auth/middleware/acl.auth");
+const bearer = require("../../auth/middleware/bearer.auth");
+const specificity = require("../../auth/middleware/specificity.auth");
+const head = require("../../auth/middleware/head");
+const {
+  coursesModel,
+  departmentsModel,
+  usersModel,
+  studentsModel,
+  institutionModel,
+  instructorsModel,
+} = require("../../model/relations");
 // departmentsRouter.get('/departments', handleGetAll);
-departmentsRouter.get('/departmentcourses/:id',bearer,acl(['admin',"instructorDepartmentHead"]),/*specificity('departmentHeader'),*/ handleGetDepartmentCourses);
-departmentsRouter.get('/departmentinstructors/:department_id',bearer,acl(['admin',"instructorDepartmentHead"]),/*specificity('departmentHeader'),*/ handleGetDepartmentInstructors);
-departmentsRouter.get('/departmentstudents/:department_id',bearer,acl(['admin',"instructorDepartmentHead"]),/*specificity('departmentHeader'),*/ handleGetDepartmentStudents);
-departmentsRouter.get('/departmentinstitution/:institutionId',bearer,acl(['admin',"instructorDepartmentHead"]),/*specificity('departmentHeader'),*/ handleGetDepartmentInstitution);
-departmentsRouter.get('/department/:department_id',bearer,acl(['admin',"instructorDepartmentHead"]),/*specificity('departmentHeader'),*/ handleGetOne);
-departmentsRouter.post('/department',bearer,acl(['admin']), handleCreate);
-departmentsRouter.put('/department/:id',bearer,acl(['admin',"instructorDepartmentHead"]),/*specificity('departmentHeader'),*/ handleUpdate);
+
+departmentsRouter.get(
+  "/departmentcourses/:id",
+  bearer,
+  acl(["admin", "instructorDepartmentHead"]),
+  /*specificity('departmentHeader'),*/ handleGetDepartmentCourses
+);
+departmentsRouter.get(
+  "/departmentinstructors/:department_id",
+  bearer,
+  acl(["admin", "instructorDepartmentHead"]),
+  /*specificity('departmentHeader'),*/ handleGetDepartmentInstructors
+);
+departmentsRouter.get(
+  "/departmentstudents/:department_id",
+  bearer,
+  acl(["admin", "instructorDepartmentHead"]),
+  /*specificity('departmentHeader'),*/ handleGetDepartmentStudents
+);
+departmentsRouter.get(
+  "/departmentinstitution/:institutionId",
+  bearer,
+  acl(["admin", "instructorDepartmentHead"]),
+  /*specificity('departmentHeader'),*/ handleGetDepartmentInstitution
+);
+departmentsRouter.get(
+  "/department/:department_id",
+  bearer,
+  acl(["admin", "instructorDepartmentHead"]),
+  /*specificity('departmentHeader'),*/ handleGetOne
+);
+departmentsRouter.post("/department", bearer, acl(["admin"]), handleCreate);
+departmentsRouter.put(
+  "/department/:id",
+  bearer,
+  acl(["admin", "instructorDepartmentHead"]),
+  /*specificity('departmentHeader'),*/ handleUpdate
+);
 
 // departmentsRouter.delete('/department/:id', handleDelete);
 
@@ -23,67 +61,85 @@ departmentsRouter.put('/department/:id',bearer,acl(['admin',"instructorDepartmen
 // }
 
 async function handleGetDepartmentCourses(req, res) {
-  try{
-  let allRecords = await coursesModel.findAndCountAll(
-    {
-      where:{departmentId:req.params.id},
-      attributes:['id','name',"description","start_date","end_date"],
-      
+  try {
+    let allRecords = await coursesModel.findAndCountAll({
+      where: { departmentId: req.params.id },
+      attributes: ["id", "name", "description", "start_date", "end_date"],
     });
-  res.status(200).json(allRecords);
-} catch (e){next(e)}
+    res.status(200).json(allRecords);
+  } catch (e) {
+    next(e);
+  }
 }
 
 async function handleGetDepartmentInstructors(req, res) {
-  try{
-    let id =req.params.department_id;
-  let allRecords = await instructorsModel.findAndCountAll({where:{departmentId:id}});
-  res.status(200).json(allRecords);
-} catch (e){next(e)}
+  try {
+    let id = req.params.department_id;
+    let allRecords = await instructorsModel.findAndCountAll({
+      where: { departmentId: id },
+    });
+    res.status(200).json(allRecords);
+  } catch (e) {
+    next(e);
+  }
 }
 
 async function handleGetDepartmentStudents(req, res) {
-  try{
-    let id=req.params.department_id
-  let allRecords = await studentsModel.findAndCountAll({where:{departmentId:id}});
-  res.status(200).json(allRecords);
-} catch (e){next(e)}
+  try {
+    let id = req.params.department_id;
+    let allRecords = await studentsModel.findAndCountAll({
+      where: { departmentId: id },
+    });
+    res.status(200).json(allRecords);
+  } catch (e) {
+    next(e);
+  }
 }
 
 async function handleGetDepartmentInstitution(req, res) {
-  try{
-    let id=req.params.institutionId
-  let allRecords = await institutionModel.findAndCountAll({where:{institutionId:id}});
-  res.status(200).json(allRecords);
-} catch (e){next(e)}
+  try {
+    let id = req.params.institutionId;
+    let allRecords = await institutionModel.findAndCountAll({
+      where: { institutionId: id },
+    });
+    res.status(200).json(allRecords);
+  } catch (e) {
+    next(e);
+  }
 }
 
 async function handleGetOne(req, res) {
-  try{
-  const id = req.params.department_id;
-  let theRecord = await departmentsModel.findOne(
-    {where:{id:id},
-    include:[
-      {model:instructorsModel,as:'department_head',}]})
-  res.status(200).json(theRecord);
-} catch (e){next(e)}
+  try {
+    const id = req.params.department_id;
+    let theRecord = await departmentsModel.findOne({
+      where: { id: id },
+      include: [{ model: instructorsModel, as: "departmentHead" }],
+    });
+    res.status(200).json(theRecord);
+  } catch (e) {
+    next(e);
+  }
 }
 
-async function handleCreate(req, res,next) {
-  try{
-  let obj = req.body;
-  let newRecord = await departmentsModel.create(obj);
-  res.status(201).json(newRecord);
-} catch (e){next(e)}
+async function handleCreate(req, res, next) {
+  try {
+    let obj = req.body;
+    let newRecord = await departmentsModel.create(obj);
+    res.status(201).json(newRecord);
+  } catch (e) {
+    next(e);
+  }
 }
 
-async function handleUpdate(req, res,next) {
-  try{
-  const id = req.params.id;
-  const obj = req.body;
-  let updatedRecord = await departmentsModel.findOne({where:{id}})
-  res.status(200).json(await updatedRecord.update(obj));
-} catch (e){next(e)}
+async function handleUpdate(req, res, next) {
+  try {
+    const id = req.params.id;
+    const obj = req.body;
+    let updatedRecord = await departmentsModel.findOne({ where: { id } });
+    res.status(200).json(await updatedRecord.update(obj));
+  } catch (e) {
+    next(e);
+  }
 }
 
 // async function handleDelete(req, res) {
@@ -91,6 +147,5 @@ async function handleUpdate(req, res,next) {
 //   let deletedRecord = await departmentsModel.destroy({where:{id:id}});
 //   res.status(204).json(deletedRecord);
 // }
-
 
 module.exports = departmentsRouter;
