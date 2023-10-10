0756e0121e09c217a04c7c2bc09238d0055bf8e3
Mohammad-Keath <m.ghyath.z@gmail.com>
2023-10-10 21:39:39 +0000

    user

--- src/auth/handlers/signin-handler.js
+++ src/auth/handlers/signin-handler.js
@@ -19,7 +19,7 @@ async function signInHandler(req, res) {
   ) {
     user = await usersModel.findOne({
       where: { email: req.user.email },
-      include: { model: instructorsModel,include: { all: true } },
+      include:{all:true,nested: true },
       
     });
   } else if (req.user.role === "admin" || req.user.role === "superAdmin") {
